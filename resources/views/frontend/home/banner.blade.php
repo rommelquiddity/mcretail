936f941677a6339d620059fda75d7d30d53d3e30
Rommel Anaya <rommel.anaya@quiddity.ph>
2018-08-01 20:02:53 +0800

    menu

---
+++ resources/views/frontend/home/banner.blade.php
@@ -0,0 +1,5 @@
+<div id="hero-banner">
+	<div class="container">
+		<img src="{{ asset('/images/home-hero-banner.jpg') }}" />
+	</div>
+</div>
--- resources/views/frontend/layout/header-menu.blade.php
+++ resources/views/frontend/layout/header-menu.blade.php
@@ -5,29 +5,31 @@
 
 <header>
 	<div class="transparent-bg">
-		<div id="logo">
-			<h1>
-				<a href="{{ url('/') }}">
-					<span class="alt-text">Mccormick Retail</span>
-				</a>
-			</h1>
-		</div>
-		<div id="main-menu">
-			<nav>
-				<ul>
-					<li><a href="">Products</a></li>
-					<li><a href="">Recipes</a>
-						<ul class="sub-menu">
-							<li>Glossary of terms</li>
-							<li>Tips and Technique</li>
-							<li>Food Plating</li>
-							<li>Explore Flavors</li>
-						</ul>
-					</li>
-					<li><a href="">Cooking Tips</a></li>
-					<li><a href="">Contact Us</a></li>
-				</ul>
-			</nav>
+		<div class="container">
+			<div id="logo">
+				<h1>
+					<a href="{{ url('/') }}">
+						<span class="alt-text">Mccormick Retail</span>
+					</a>
+				</h1>
+			</div>
+			<div id="main-menu">
+				<nav>
+					<ul>
+						<li><a href="">Products</a></li>
+						<li><a href="">Recipes</a>
+							<ul class="sub-menu">
+								<li><a href="">Glossary of terms</a></li>
+								<li><a href="">Tips and Technique</a></li>
+								<li><a href="">Food Plating</a></li>
+								<li><a href="">Explore Flavors</a></li>
+							</ul>
+						</li>
+						<li><a href="">Cooking Tips</a></li>
+						<li><a href="">Contact Us</a></li>
+					</ul>
+				</nav>
+			</div>
 		</div>
 	</div>
 </header>
