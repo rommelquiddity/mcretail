cbfaee8ff83d06a538979f20fa2bb9449130d26d
rommel <rommel.anaya@quiddity.ph>
2018-08-03 01:36:31 +0800

    fix footer

--- resources/views/frontend/layout/footer-menu.blade.php
+++ resources/views/frontend/layout/footer-menu.blade.php
@@ -1,18 +1,33 @@
 <footer>
-	<div class="cont">
+	<div class="container">
+
 		<div class="row">
-			<div class="col-md-4">
-				<h6>About Us</h6>
+			<div class="col-md-2">
+				<img class="footer-logo" src="{{ asset('/images/logo.png') }}" />
 			</div>
 			<div class="col-md-4">
-				<h6>News</h6>
+				<h4>About Us</h4>
+				<ul class="navigation">
+					<li><a href="">Our Company</a></li>
+					<li><a href="">Awards</a></li>
+					<li><a href="">How Our Products are Packed</a></li>
+				</ul>
 			</div>
-			<div class="col-md-4 follow">
-				<h6>PRIVACY POLICY</h6>
-				<h6>TERMS AND CONDITIONS</h6>
+			<div class="col-md-3">
+				<h4>News</h4>
+				<ul class="navigation">
+					<li><a href="">Events</a></li>
+					<li><a href="">Promos</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul class="navigation no-margin-top">
+					<li><a href="">Privacy Policy</a></li>
+					<li><a href="">Terms and Conditions</a></li>
+				</ul>
 			</div>
 		</div>
-		<div class="row bot">
+		<div class="row">
 			<div class="copyright"> 
 				&copy; COPYRIGHT 2018. ALL RIGHTS RESERVED.
 			</div>
--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -12,6 +12,8 @@
     <meta property="og:url" content="{{ url()->current() }}"/>
     <meta property="og:type" content="website"/>
 
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
     @yield('additional_head')
